96952fc244dc563835e5750584a19fb25ca208ad
Soullose <915219013@qq.com>
2023-11-08 23:12:24 +0800

    添加用户角色

---
+++ src/main/java/com/w2/springtemplate/model/SysRole.java
@@ -0,0 +1,19 @@
+package com.w2.springtemplate.model;
+
+import com.w2.springtemplate.jpa.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Table(name = "t_sys_role")
+@Entity
+public class SysRole extends BaseEntity implements Serializable {
+
+    private String roleCode;
+    private String roleName;
+}
---
+++ src/main/java/com/w2/springtemplate/model/SysUser.java
@@ -0,0 +1,22 @@
+package com.w2.springtemplate.model;
+
+import com.w2.springtemplate.jpa.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Table(name = "t_sys_user")
+@Entity
+public class SysUser extends BaseEntity implements Serializable {
+    /// 登录账户,唯一.
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String name;
+}
