84d98d9bc52a675bd025834b28fee8a12603c2cd
Soullose <915219013@qq.com>
2023-11-09 18:29:21 +0800

    调整

--- src/main/java/com/w2/springtemplate/config/ShiroConfiguration.java
+++ src/main/java/com/w2/springtemplate/config/ShiroConfiguration.java
@@ -1,16 +1,21 @@
 package com.w2.springtemplate.config;
 
+import com.w2.springtemplate.utils.crypto.BCryptPasswordEncoder;
+import com.w2.springtemplate.utils.crypto.PasswordEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.w2.springtemplate.utils.crypto.BCryptPasswordEncoder;
-import com.w2.springtemplate.utils.crypto.PasswordEncoder;
-
 @Configuration
 public class ShiroConfiguration {
-    /// 密码加密设置
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	/// 密码加密设置
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	// @Bean
+	// public Realm userAccountRealm() {
+	// return new UserAccountRealm();
+	// }
+
 }
