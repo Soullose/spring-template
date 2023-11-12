9fc3b655446b4ca251a4022ab5898961800ae268
wsf <wsf@DESKTOP-6D4R823>
2023-11-12 22:10:30 +0800

    添加配置项

--- src/main/java/com/w2/springtemplate/framework/security/config/SecurityConfig.java
+++ src/main/java/com/w2/springtemplate/framework/security/config/SecurityConfig.java
@@ -2,9 +2,15 @@ package com.w2.springtemplate.framework.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * Spring Security 配置
@@ -16,5 +22,18 @@ public class SecurityConfig {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService() {
+		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+		UserDetails user = User.withUsername("wxf").password("123456").authorities("read").build();
+		userDetailsService.createUser(user);
+		return userDetailsService;
+	}
+	
+	@Bean
+	AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+		return configuration.getAuthenticationManager();
 	}
 }
