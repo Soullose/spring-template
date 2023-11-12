352dcd704e9f3085aad79f9864fc999a9be9029f
WangSF <soullose@qq.com>
2023-11-12 17:31:47 +0800

    去除剩余引入

--- src/main/java/com/w2/springtemplate/controller/LoginController.java
+++ src/main/java/com/w2/springtemplate/controller/LoginController.java
@@ -1,10 +1,10 @@
 package com.w2.springtemplate.controller;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.subject.Subject;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/login")
@@ -12,10 +12,6 @@ public class LoginController {
 
     @PostMapping
     public ResponseEntity login(@RequestParam String password){
-        UsernamePasswordToken token = new
-                UsernamePasswordToken("user", password);
-        Subject subject = SecurityUtils.getSubject();
-        subject.login(token);
         return ResponseEntity.ok().build();
     }
 }
