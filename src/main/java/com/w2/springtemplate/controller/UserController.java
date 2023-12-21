31286a98e2ee405f7bc4fb9988571b73ff8bfbde
Soullose <915219013@qq.com>
2023-12-21 20:37:36 +0800

    调整

--- src/main/java/com/w2/springtemplate/controller/UserController.java
+++ src/main/java/com/w2/springtemplate/controller/UserController.java
@@ -45,13 +45,6 @@ public class UserController {
 	@ApiOperation(value = "校验密码")
 	@GetMapping("/check")
 	public ResponseEntity<SysUserDTO> check(@RequestParam String username, @RequestParam String password) {
-//		QSysUser qSysUser = QSysUser.sysUser;
-//		Predicate predicate = qSysUser.username.eq(username);
-//		SysUser sysUser = sysUserRepository.findOne(predicate).orElse(null);
-//		String password1 = sysUser.getPassword();
-//		boolean matches = passwordEncoder.matches(password, password1);
-		// log.info("校验密码是否正确:{}",OpenBSDBCrypt.checkPassword(password1,password.toCharArray()));
-//		log.info("密码是否正确:{}", matches);
 		return ResponseEntity.ok(runEnvironment.run(CheckPasswordCommand.builder().username(username).password(password).build()));
 	}
 
