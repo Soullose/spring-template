3857fa25f3a9e0de3c9adae7621f344a8de6a45e
Soullose <915219013@qq.com>
2024-01-04 11:29:30 +0800

    测试netty

---
+++ src/main/java/com/w2/springtemplate/framework/netty/test/ModbusClient.java
@@ -0,0 +1,80 @@
+package com.w2.springtemplate.framework.netty.test;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ModbusClient {
+    public static void main(String[] args) {
+
+        byte[] bytes = {
+                0x00, 0x03,
+                0x00, 0x00,
+                0x00, 0x06,
+                0x02,
+                0x03,
+                0x00, 0x00,
+                0x00, (byte) 0x0a
+        };
+
+
+        ByteBuf buf = ByteBufAllocator.DEFAULT.buffer(12);
+        buf.writeBytes(bytes);
+
+        Bootstrap bootstrap = new Bootstrap()
+                .group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<Channel>() {
+                    @Override
+                    protected void initChannel(Channel channel) throws Exception {
+                        channel.pipeline().addLast("h1",new ChannelInboundHandlerAdapter(){
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                log2((ByteBuf)msg);
+                                log.debug("{}",(ByteBuf)msg);
+                                Thread.sleep(1500);
+                                ctx.writeAndFlush(ctx.alloc().buffer().writeBytes(bytes));
+                            }
+                        });
+                    }
+                });
+
+        ChannelFuture connect = bootstrap.connect("127.0.0.1", 502);
+
+        connect.addListener(new ChannelFutureListener(){
+            @Override
+            public void operationComplete(ChannelFuture future) throws Exception {
+                future.channel().writeAndFlush(buf);
+            }
+        });
+    }
+
+
+    private static void log(ByteBuf buffer){
+        int length = buffer.readableBytes();
+        int rows = length / 16 + (length % 15== 0 ? 0 : 1) + 4;
+        StringBuffer stringBuffer = new StringBuffer(rows * 80 * 2)
+                .append("read index:").append(buffer.readerIndex())
+                .append("write index:").append(buffer.writerIndex())
+                .append("capacity:").append(buffer.capacity());
+
+    }
+
+
+    private static void log2(ByteBuf buffer){
+        String s = ByteBufUtil.hexDump(buffer);
+        byte len = buffer.getByte(8);
+        log.debug("接受到的Modbus 报文:{}",s);
+        log.debug("接受到的Modbus 寄存器长度:{}",len);
+
+        for (int i = 0; i < len; i+=2) {
+            log.debug("值:{}",buffer.getShort(9+i));
+        }
+    }
+}
