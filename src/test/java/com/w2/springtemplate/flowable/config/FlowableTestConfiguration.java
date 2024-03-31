a15f57ffedb53805f9265dadff7970580ce45a81
WangSF <soullose@qq.com>
2024-03-31 14:47:28 +0800

    add flowableEngine 6.8.1

---
+++ src/test/java/com/w2/springtemplate/flowable/config/FlowableTestConfiguration.java
@@ -0,0 +1,11 @@
+package com.w2.springtemplate.flowable.config;
+
+import org.junit.jupiter.api.Test;
+
+public class FlowableTestConfiguration {
+
+    @Test
+    public void test01() {
+
+    }
+}
