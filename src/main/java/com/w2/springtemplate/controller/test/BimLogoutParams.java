a4000a6d518682c533ef787c34aac4a650b3b2a6
WangSF <soullose@qq.com>
2024-09-20 17:43:23 +0800

    添加测试代码

--- src/main/java/com/w2/springtemplate/controller/test/BimLogoutParams.java
+++ src/main/java/com/w2/springtemplate/controller/test/BimLogoutParams.java
@@ -2,9 +2,9 @@ package com.w2.springtemplate.controller.test;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 public class BimLogoutParams {
     private String token;
 }
