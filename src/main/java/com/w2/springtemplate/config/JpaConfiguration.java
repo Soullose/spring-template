7e6f1411b72e192afbee27751238a8204091923f
WangSF <soullose@qq.com>
2025-01-19 18:36:43 +0800

    feat(config): 优化 JPA配置并添加自定义仓库支持
    - 在 JpaConfiguration 中添加 CustomRepositoryFactoryBean 支持- 更新 JPAQueryFactory 构造方式，使用 PersistenceContext 注解
    - 升级 apache-shiro 版本至 2.0.2
    - 设置 Maven 编译参数以使用 Java 11
    - 添加 Google Tink 依赖

--- src/main/java/com/w2/springtemplate/config/JpaConfiguration.java
+++ src/main/java/com/w2/springtemplate/config/JpaConfiguration.java
@@ -1,12 +1,16 @@
 package com.w2.springtemplate.config;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
+import com.w2.springtemplate.framework.jpa.CustomRepositoryFactoryBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.jpa.repository.support.JpaRepositoryFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -14,12 +18,17 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 @Configuration
 @EnableJpaAuditing()
 @EnableTransactionManagement
+@EnableJpaRepositories(repositoryFactoryBeanClass = CustomRepositoryFactoryBean.class, basePackages = {
+		"com.w2.springtemplate.infrastructure.repository" })
 public class JpaConfiguration {
-    private static final Logger log = LoggerFactory.getLogger(JpaConfiguration.class);
+	private static final Logger log = LoggerFactory.getLogger(JpaConfiguration.class);
 
-    @Bean
-    public JPAQueryFactory queryFactory(EntityManager em) {
-        log.debug("创建JPAQueryFactory");
-        return new JPAQueryFactory(em);
-    }
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Bean
+	public JPAQueryFactory queryFactory() {
+		log.debug("创建JPAQueryFactory");
+		return new JPAQueryFactory(entityManager);
+	}
 }
