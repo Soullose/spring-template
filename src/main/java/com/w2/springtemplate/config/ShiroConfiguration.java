5bb6f2a40d226ac072b91f507947b8e453b486e9
WangSF <soullose@qq.com>
2025-02-25 12:39:24 +0800

    refactor(shiro): 重构 Shiro 配置和 JWT 认证逻辑
    - 优化了 BearerRealm 类的结构和逻辑- 重构了 JwtUtil 类,使用更安全的密钥生成方式
    - 调整了 ShiroConfiguration 类的配置方式
    - 删除了不必要的注释和冗余代码
    - 优化了代码格式和缩进

--- src/main/java/com/w2/springtemplate/config/ShiroConfiguration.java
+++ src/main/java/com/w2/springtemplate/config/ShiroConfiguration.java
@@ -1,12 +1,16 @@
 package com.w2.springtemplate.config;
 
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-
-import javax.servlet.DispatcherType;
-import javax.servlet.Filter;
-
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.w2.springtemplate.framework.shiro.authc.MultipleRealmAuthentic;
 import com.w2.springtemplate.framework.shiro.cache.ShiroRedisCacheManager;
+import com.w2.springtemplate.framework.shiro.extention.RetryPasswordCredentialsMatcher;
+import com.w2.springtemplate.framework.shiro.filter.BearerAuthenticFilter;
+import com.w2.springtemplate.framework.shiro.filter.UserAccountLoginFilter;
+import com.w2.springtemplate.framework.shiro.realm.BearerRealm;
+import com.w2.springtemplate.framework.shiro.realm.UserAccountRealm;
+import com.w2.springtemplate.framework.shiro.session.NoSessionWebSubjectFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.authc.Authenticator;
 import org.apache.shiro.authc.BearerToken;
 import org.apache.shiro.authc.UsernamePasswordToken;
@@ -15,11 +19,8 @@ import org.apache.shiro.authc.credential.DefaultPasswordService;
 import org.apache.shiro.authc.credential.PasswordService;
 import org.apache.shiro.authc.pam.FirstSuccessfulStrategy;
 import org.apache.shiro.cache.CacheManager;
-import org.apache.shiro.mgt.DefaultSessionStorageEvaluator;
-import org.apache.shiro.mgt.DefaultSubjectDAO;
 import org.apache.shiro.mgt.SecurityManager;
-import org.apache.shiro.mgt.SessionStorageEvaluator;
-import org.apache.shiro.mgt.SubjectDAO;
+import org.apache.shiro.mgt.*;
 import org.apache.shiro.realm.Realm;
 import org.apache.shiro.session.mgt.DefaultSessionManager;
 import org.apache.shiro.session.mgt.SessionManager;
@@ -36,225 +37,222 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.web.filter.DelegatingFilterProxy;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.w2.springtemplate.framework.shiro.authc.MultipleRealmAuthentic;
-import com.w2.springtemplate.framework.shiro.extention.RetryPasswordCredentialsMatcher;
-import com.w2.springtemplate.framework.shiro.filter.BearerAuthenticFilter;
-import com.w2.springtemplate.framework.shiro.filter.UserAccountLoginFilter;
-import com.w2.springtemplate.framework.shiro.realm.BearerRealm;
-import com.w2.springtemplate.framework.shiro.realm.UserAccountRealm;
-import com.w2.springtemplate.framework.shiro.session.NoSessionWebSubjectFactory;
-
-import lombok.extern.slf4j.Slf4j;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 
 @Slf4j
 @Configuration
 public class ShiroConfiguration {
 
+    /**
+     * 管理shiro的生命周期
+     */
+    @Bean
+    public static LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+        return new LifecycleBeanPostProcessor();
+    }
 
+    /// 密码加密设置
+    // @Bean
+    // public PasswordEncoder passwordEncoder() {
+    // return new BCryptPasswordEncoder();
+    // }
+    //
+    // @Bean
+    // public PasswordService passwordService() {
+    // return new BCryptPasswordService(passwordEncoder());
+    // }
+    //
+    // @Bean
+    // public CredentialsMatcher passwordMatcher() {
+    // return new BCryptPasswordMatcher(passwordService());
+    // }
+    @Bean
+    public PasswordService defaultPasswordService() {
+        return new DefaultPasswordService();
+    }
 
-	@Bean
-	LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
-		return new LifecycleBeanPostProcessor();
-	}
+    // @Bean
+    // public CredentialsMatcher passwordMatcher() {
+    // return new PasswordMatcher();
+    // }
 
-	/// 密码加密设置
-	// @Bean
-	// public PasswordEncoder passwordEncoder() {
-	// return new BCryptPasswordEncoder();
-	// }
-	//
-	// @Bean
-	// public PasswordService passwordService() {
-	// return new BCryptPasswordService(passwordEncoder());
-	// }
-	//
-	// @Bean
-	// public CredentialsMatcher passwordMatcher() {
-	// return new BCryptPasswordMatcher(passwordService());
-	// }
+    @Bean
+    public CredentialsMatcher passwordMatcher() {
+        return new RetryPasswordCredentialsMatcher();
+    }
 
-	@Bean
-	public PasswordService defaultPasswordService() {
-		return new DefaultPasswordService();
-	}
+    @Bean(name = "shiroCacheManager")
+    CacheManager shiroCacheManager() {
+        return new ShiroRedisCacheManager();
+    }
 
-	// @Bean
-	// public CredentialsMatcher passwordMatcher() {
-	// return new PasswordMatcher();
-	// }
+    /// 关闭shiro自带session
+    @Bean
+    SessionManager sessionManager() {
+        DefaultSessionManager sessionManager = new DefaultSessionManager();
+        sessionManager.setSessionValidationSchedulerEnabled(false);
+        return sessionManager;
+    }
 
-	@Bean
-	public CredentialsMatcher passwordMatcher() {
-		return new RetryPasswordCredentialsMatcher();
-	}
+    /// 关闭shiro自带session
+    @Bean
+    SessionStorageEvaluator sessionStorageEvaluator() {
+        DefaultSessionStorageEvaluator sessionStorageEvaluator = new DefaultSessionStorageEvaluator();
+        sessionStorageEvaluator.setSessionStorageEnabled(false);
+        return sessionStorageEvaluator;
+    }
 
-	@Bean(name = "shiroCacheManager")
-	CacheManager shiroCacheManager() {
-		return new ShiroRedisCacheManager();
-	}
+    /// 关闭shiro自带session
+    @Bean
+    SubjectDAO defaultSubjectDAO() {
+        DefaultSubjectDAO defaultSubjectDAO = new DefaultSubjectDAO();
+        defaultSubjectDAO.setSessionStorageEvaluator(sessionStorageEvaluator());
+        return defaultSubjectDAO;
+    }
 
-	/// 关闭shiro自带session
-	@Bean
-	SessionManager sessionManager() {
-		DefaultSessionManager sessionManager = new DefaultSessionManager();
-		sessionManager.setSessionValidationSchedulerEnabled(false);
-		return sessionManager;
-	}
+    @Bean
+    FilterRegistrationBean<Filter> shiroFilterRegistrationBean() {
+        FilterRegistrationBean<Filter> filterRegistration = new FilterRegistrationBean<>();
+        filterRegistration.setFilter(new DelegatingFilterProxy("shiroFilterFactoryBean"));
+        filterRegistration.setEnabled(true);
+        filterRegistration.addUrlPatterns("/*");
+        filterRegistration.setDispatcherTypes(DispatcherType.REQUEST);
+        return filterRegistration;
+    }
 
-	/// 关闭shiro自带session
-	@Bean
-	SessionStorageEvaluator sessionStorageEvaluator() {
-		DefaultSessionStorageEvaluator sessionStorageEvaluator = new DefaultSessionStorageEvaluator();
-		sessionStorageEvaluator.setSessionStorageEnabled(false);
-		return sessionStorageEvaluator;
-	}
+    /// 自定义过滤器
+    @Bean
+    LinkedHashMap<String, Filter> filters() {
+        LinkedHashMap<String, Filter> filters = Maps.newLinkedHashMap();
+        filters.put("jwt", new BearerAuthenticFilter());
+        filters.put("login", new UserAccountLoginFilter());
+        return filters;
+    }
 
-	/// 关闭shiro自带session
-	@Bean
-	SubjectDAO defaultSubjectDAO() {
-		DefaultSubjectDAO defaultSubjectDAO = new DefaultSubjectDAO();
-		defaultSubjectDAO.setSessionStorageEvaluator(sessionStorageEvaluator());
-		return defaultSubjectDAO;
-	}
+    /// 路径匹配规则
+    @Bean
+    LinkedHashMap<String, String> filterChainDefinitionMap() {
+        LinkedHashMap<String, String> filterChainDefinitionMap = Maps.newLinkedHashMap();
+        filterChainDefinitionMap.put("/api/login", "login");
+        filterChainDefinitionMap.put("/doc.html", "anon");
+        filterChainDefinitionMap.put("/user/register", "anon");
+        filterChainDefinitionMap.put("/api/**", "jwt");
+        filterChainDefinitionMap.put("/test/**", "anon");
+        filterChainDefinitionMap.put("/testEvent/**", "anon");
+        filterChainDefinitionMap.put("/bim-server/**", "anon");
+        filterChainDefinitionMap.put("/**", "anon");
+        return filterChainDefinitionMap;
+    }
 
-	@Bean
-	FilterRegistrationBean<Filter> shiroFilterRegistrationBean() {
-		FilterRegistrationBean<Filter> filterRegistration = new FilterRegistrationBean<>();
-		filterRegistration.setFilter(new DelegatingFilterProxy("shiroFilterFactoryBean"));
-		filterRegistration.setEnabled(true);
-		filterRegistration.addUrlPatterns("/*");
-		filterRegistration.setDispatcherTypes(DispatcherType.REQUEST);
-		return filterRegistration;
-	}
-
-	/// 自定义过滤器
-	@Bean
-	LinkedHashMap<String, Filter> filters() {
-		LinkedHashMap<String, Filter> filters = Maps.newLinkedHashMap();
-		filters.put("jwt", new BearerAuthenticFilter());
-		filters.put("login", new UserAccountLoginFilter());
-		return filters;
-	}
-
-	/// 路径匹配规则
-	@Bean
-	LinkedHashMap<String, String> filterChainDefinitionMap() {
-		LinkedHashMap<String, String> filterChainDefinitionMap = Maps.newLinkedHashMap();
-		filterChainDefinitionMap.put("/api/login", "login");
-		filterChainDefinitionMap.put("/doc.html", "anon");
-		filterChainDefinitionMap.put("/user/register", "anon");
-		filterChainDefinitionMap.put("/api/**", "jwt");
-		filterChainDefinitionMap.put("/test/**", "anon");
-		filterChainDefinitionMap.put("/testEvent/**", "anon");
-		filterChainDefinitionMap.put("/bim-server/**", "anon");
-		filterChainDefinitionMap.put("/**", "anon");
-		return filterChainDefinitionMap;
-	}
-
-	@Bean
-	Realm userAccountRealm() {
-		UserAccountRealm userAccountRealm = new UserAccountRealm();
-		userAccountRealm.setCredentialsMatcher(passwordMatcher());
-		userAccountRealm.setAuthenticationTokenClass(UsernamePasswordToken.class);
+    @Bean
+    Realm userAccountRealm() {
+        UserAccountRealm userAccountRealm = new UserAccountRealm();
+        userAccountRealm.setCredentialsMatcher(passwordMatcher());
+        userAccountRealm.setAuthenticationTokenClass(UsernamePasswordToken.class);
         userAccountRealm.setCachingEnabled(true);
         userAccountRealm.setAuthenticationCachingEnabled(true);
         userAccountRealm.setAuthorizationCachingEnabled(true);
         userAccountRealm.setAuthenticationCacheName("authenticationCache");
         userAccountRealm.setAuthorizationCacheName("authorizationCache");
-		userAccountRealm.setCacheManager(shiroCacheManager());
-		return userAccountRealm;
-	}
+        userAccountRealm.setCacheManager(shiroCacheManager());
+        return userAccountRealm;
+    }
 
-	@Bean
-	Realm bearerRealm() {
-		BearerRealm bearerRealm = new BearerRealm();
-		bearerRealm.setAuthenticationTokenClass(BearerToken.class);
-		return bearerRealm;
-	}
+    @Bean
+    Realm bearerRealm() {
+        BearerRealm bearerRealm = new BearerRealm();
+        bearerRealm.setAuthenticationTokenClass(BearerToken.class);
+        return bearerRealm;
+    }
 
-	@Bean
-	Authenticator multipleRealmAuthentic(Realm bearerRealm, Realm userAccountRealm) {
-		MultipleRealmAuthentic multipleRealmAuthentic = new MultipleRealmAuthentic();
-		multipleRealmAuthentic.setRealms(Arrays.asList(bearerRealm, userAccountRealm));
-		multipleRealmAuthentic.setAuthenticationStrategy(new FirstSuccessfulStrategy());
-		return multipleRealmAuthentic;
+    @Bean
+    Authenticator multipleRealmAuthentic(Realm bearerRealm, Realm userAccountRealm) {
+        MultipleRealmAuthentic multipleRealmAuthentic = new MultipleRealmAuthentic();
+        multipleRealmAuthentic.setRealms(Arrays.asList(bearerRealm, userAccountRealm));
+        multipleRealmAuthentic.setAuthenticationStrategy(new FirstSuccessfulStrategy());
+        return multipleRealmAuthentic;
 
-		// ModularRealmAuthenticator authenticator = new ModularRealmAuthenticator(); //
-		// MultiRealmAuthenticator
-		// 设置两个Realm，一个用于用户登录验证和访问权限获取；一个用于jwt token的认证
-		// authenticator.setRealms(Arrays.asList(bearerRealm, userAccountRealm));
-		// 设置多个realm认证策略，一个成功即跳过其它的
-		// authenticator.setAuthenticationStrategy(new FirstSuccessfulStrategy());
-		// return authenticator;
-	}
+        // ModularRealmAuthenticator authenticator = new ModularRealmAuthenticator(); //
+        // MultiRealmAuthenticator
+        // 设置两个Realm，一个用于用户登录验证和访问权限获取；一个用于jwt token的认证
+        // authenticator.setRealms(Arrays.asList(bearerRealm, userAccountRealm));
+        // 设置多个realm认证策略，一个成功即跳过其它的
+        // authenticator.setAuthenticationStrategy(new FirstSuccessfulStrategy());
+        // return authenticator;
+    }
 
-	@Bean
-	public DefaultWebSecurityManager shiroSecurityManager(Realm userAccountRealm, Realm bearerRealm,
-			Authenticator multipleRealmAuthentic) {
-		DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
-		manager.setCacheManager(shiroCacheManager());
-		// manager.setSessionManager(sessionManager());
-		// manager.setSubjectDAO(defaultSubjectDAO());
-		manager.setSubjectFactory(new NoSessionWebSubjectFactory()); // 使用无状态的工厂
-		// 默认SubjectDAO会写入会话，无状态时需要通过以下代码阻止写入
-		DefaultSubjectDAO subjectDAO = (DefaultSubjectDAO) manager.getSubjectDAO();
-		DefaultSessionStorageEvaluator sessionStorageEvaluator = (DefaultSessionStorageEvaluator) subjectDAO
-				.getSessionStorageEvaluator();
-		sessionStorageEvaluator.setSessionStorageEnabled(false);
+    @Bean
+    public DefaultWebSecurityManager shiroSecurityManager(Realm userAccountRealm, Realm bearerRealm,
+                                                          Authenticator multipleRealmAuthentic) {
+        DefaultWebSecurityManager manager = new DefaultWebSecurityManager();
+        manager.setCacheManager(shiroCacheManager());
+        // manager.setSessionManager(sessionManager());
+        // manager.setSubjectDAO(defaultSubjectDAO());
+        manager.setSubjectFactory(new NoSessionWebSubjectFactory()); // 使用无状态的工厂
+        // 默认SubjectDAO会写入会话，无状态时需要通过以下代码阻止写入
+        DefaultSubjectDAO subjectDAO = (DefaultSubjectDAO) manager.getSubjectDAO();
+        DefaultSessionStorageEvaluator sessionStorageEvaluator = (DefaultSessionStorageEvaluator) subjectDAO
+                .getSessionStorageEvaluator();
+        sessionStorageEvaluator.setSessionStorageEnabled(false);
 
-		// 加入AccountRealm
-		// manager.setRealms(Lists.newArrayList(userAccountRealm()));
-		// 加入AccountRealm
-		manager.setRealms(Lists.newArrayList(userAccountRealm));
-		manager.setAuthenticator(multipleRealmAuthentic);
-		return manager;
-	}
+        // 加入AccountRealm
+        // manager.setRealms(Lists.newArrayList(userAccountRealm()));
+        // 加入AccountRealm
+        manager.setRealms(Lists.newArrayList(userAccountRealm));
+        manager.setAuthenticator(multipleRealmAuthentic);
+        return manager;
+    }
 
-	@Bean
-	public ShiroFilterFactoryBean shiroFilterFactoryBean(DefaultWebSecurityManager securityManager,
-			LinkedHashMap<String, Filter> filters, LinkedHashMap<String, String> filterChainDefinitionMap) {
+    @Bean
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(DefaultWebSecurityManager securityManager,
+                                                         LinkedHashMap<String, Filter> filters, LinkedHashMap<String, String> filterChainDefinitionMap) {
 
-		ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
-		shiroFilterFactoryBean.setSecurityManager(securityManager);
-		//
-		shiroFilterFactoryBean.setLoginUrl("/api/login");
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        //
+        shiroFilterFactoryBean.setLoginUrl("/api/login");
 
-		ShiroFilterConfiguration config = shiroFilterFactoryBean.getShiroFilterConfiguration();
-		config.setFilterOncePerRequest(Boolean.TRUE);
+        ShiroFilterConfiguration config = shiroFilterFactoryBean.getShiroFilterConfiguration();
+        config.setFilterOncePerRequest(Boolean.TRUE);
 
-		shiroFilterFactoryBean.setFilters(filters);
-		LinkedHashMap<String, Filter> shiroFilters = (LinkedHashMap<String, Filter>) shiroFilterFactoryBean
-				.getFilters();
-		log.info("shiroFilters--:{}", shiroFilters);
-		shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
-		log.info("拦截地址:{}", shiroFilterFactoryBean.getFilterChainDefinitionMap());
-		// 前后端分离模式下不需要
-		shiroFilterFactoryBean.setSuccessUrl(null);
-		shiroFilterFactoryBean.setUnauthorizedUrl(null);
-		return shiroFilterFactoryBean;
-	}
+        shiroFilterFactoryBean.setFilters(filters);
+        LinkedHashMap<String, Filter> shiroFilters = (LinkedHashMap<String, Filter>) shiroFilterFactoryBean
+                .getFilters();
+        log.info("shiroFilters--:{}", shiroFilters);
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+        log.info("拦截地址:{}", shiroFilterFactoryBean.getFilterChainDefinitionMap());
+        // 前后端分离模式下不需要
+        shiroFilterFactoryBean.setSuccessUrl(null);
+        shiroFilterFactoryBean.setUnauthorizedUrl(null);
+        return shiroFilterFactoryBean;
+    }
 
-	/**
-	 * 下面的代码是添加注解支持
-	 */
-	@Bean
-	@DependsOn("lifecycleBeanPostProcessor")
-	DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
-		DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
-		// 强制使用cglib，防止重复代理和可能引起代理出错的问题，https://zhuanlan.zhihu.com/p/29161098
-		defaultAdvisorAutoProxyCreator.setProxyTargetClass(true);
-		return defaultAdvisorAutoProxyCreator;
-	}
+    /**
+     * 下面的代码是添加注解支持
+     */
+    @Bean
+    @DependsOn("lifecycleBeanPostProcessor")
+    DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
+        // 强制使用cglib，防止重复代理和可能引起代理出错的问题，https://zhuanlan.zhihu.com/p/29161098
+        defaultAdvisorAutoProxyCreator.setProxyTargetClass(true);
+        return defaultAdvisorAutoProxyCreator;
+    }
 
-	@Bean
-	AuthorizationAttributeSourceAdvisor shiroAuthorizationAttributeSourceAdvisor(
-			@Autowired SecurityManager securityManager) {
-		AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
-		authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
+    /**
+     * 加入shiro注解 切点
+     */
+    @Bean
+    @DependsOn("lifecycleBeanPostProcessor")
+    AuthorizationAttributeSourceAdvisor shiroAuthorizationAttributeSourceAdvisor(
+            @Autowired SecurityManager securityManager) {
+        AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
+        authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
 
-		return authorizationAttributeSourceAdvisor;
-	}
+        return authorizationAttributeSourceAdvisor;
+    }
 
 }
--- src/main/java/com/w2/springtemplate/framework/shiro/jwt/JwtUtil.java
+++ src/main/java/com/w2/springtemplate/framework/shiro/jwt/JwtUtil.java
@@ -1,108 +1,106 @@
 package com.w2.springtemplate.framework.shiro.jwt;
 
+import com.google.common.collect.ImmutableMap;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SecureDigestAlgorithm;
+
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
 
-import javax.crypto.SecretKey;
-
-import com.google.common.collect.ImmutableMap;
-
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtBuilder;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.SecureDigestAlgorithm;
-
 public class JwtUtil {
 
-	private static final String SECRET_KET = "502B614D71315565304F482B414D64526151665144326535656A6778313372537941543063655A734B30633D";
+    private static final String SECRET_KEY = "502B614D71315565304F482B414D64526151665144326535656A6778313372537941543063655A734B30633D";
 
-	// 有效期 7天
-	public static final Long JWT_TTL = 60 * 60 * 24 * 7 * 1000L; // 60 * 60 * 1000l一个小时
-	// 设置密码明文
-	public static final String JWT_KEY = "OPEN";
+    // 有效期 7天
+    public static final Long JWT_TTL = 60 * 60 * 24 * 7 * 1000L; // 60 * 60 * 1000l一个小时
+    // 设置密码明文
+    public static final String JWT_KEY = "OPEN";
 
-	private final static SecureDigestAlgorithm<SecretKey, SecretKey> ALGORITHM = Jwts.SIG.HS256;
+    private final static SecureDigestAlgorithm<SecretKey, SecretKey> ALGORITHM = Jwts.SIG.HS256;
 
-	/**
-	 * 生成jtw(uuid)
-	 *
-	 * @param subject
-	 *            token中要存放的数据（json格式）
-	 * @return
-	 */
-	public static String createJWT(String subject, Map<String, Object> claims) {
-		JwtBuilder builder = getJwtBuilder(subject, claims, null, getUUID());// 设置过期时间
-		return builder.compact();
-	}
+    /**
+     * 生成jtw(uuid)
+     *
+     * @param subject token中要存放的数据（json格式）
+     * @return
+     */
+    public static String createJWT(String subject, Map<String, Object> claims) {
+        JwtBuilder builder = getJwtBuilder(subject, claims, null, getUUID());// 设置过期时间
+        return builder.compact();
+    }
 
-	/**
-	 * 生成jtw(uuid)
-	 *
-	 * @param subject
-	 *            token中要存放的数据（json格式）
-	 * @param ttlMillis
-	 *            token超时时间
-	 * @return
-	 */
-	public static String createJWT(String subject, Map<String, Object> claims, Long ttlMillis) {
-		JwtBuilder builder = getJwtBuilder(subject, claims, ttlMillis, getUUID());// 设置过期时间
-		return builder.compact();
-	}
+    /**
+     * 生成jtw(uuid)
+     *
+     * @param subject   token中要存放的数据（json格式）
+     * @param ttlMillis token超时时间
+     * @return
+     */
+    public static String createJWT(String subject, Map<String, Object> claims, Long ttlMillis) {
+        JwtBuilder builder = getJwtBuilder(subject, claims, ttlMillis, getUUID());// 设置过期时间
+        return builder.compact();
+    }
 
-	/**
-	 * 创建token
-	 *
-	 * @param id
-	 * @param subject
-	 * @param ttlMillis
-	 * @return
-	 */
-	public static String createJWT(String subject, Map<String, Object> claims, Long ttlMillis, String id) {
-		JwtBuilder builder = getJwtBuilder(subject, claims, ttlMillis, id);// 设置过期时间
-		return builder.compact();
-	}
+    /**
+     * 创建token
+     *
+     * @param id
+     * @param subject
+     * @param ttlMillis
+     * @return
+     */
+    public static String createJWT(String subject, Map<String, Object> claims, Long ttlMillis, String id) {
+        JwtBuilder builder = getJwtBuilder(subject, claims, ttlMillis, id);// 设置过期时间
+        return builder.compact();
+    }
 
-	public static String getUUID() {
-		return UUID.randomUUID().toString().replace("-", "");
-	}
+    public static String getUUID() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
 
-	/**
-	 * 生成加密后的秘钥 secretKey
-	 *
-	 * @return
-	 */
-	public static SecretKey generalKey() {
-		return Jwts.SIG.HS256.key().build();
-	}
+    /**
+     * 生成加密后的秘钥 secretKey
+     *
+     * @return
+     */
+    public static SecretKey generalKey() {
+//		return Jwts.SIG.HS256.key().build();
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRET_KEY));
+    }
 
-	/**
-	 * 解析
-	 *
-	 * @param jwt
-	 * @return
-	 * @throws Exception
-	 */
-	public static Claims parseJWT(String jwt) {
-		SecretKey secretKey = generalKey();
-		return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(jwt).getPayload();
-	}
+    /**
+     * 解析
+     *
+     * @param jwt
+     * @return
+     * @throws Exception
+     */
+    public static Claims parseJWT(String jwt) {
+        SecretKey secretKey = generalKey();
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(jwt).getPayload();
+    }
 
-	private static JwtBuilder getJwtBuilder(String subject, Map<String, Object> claims, Long ttlMillis, String uuid) {
-		SecretKey secretKey = generalKey();
-		long nowMillis = System.currentTimeMillis();
-		Date now = new Date(nowMillis);
-		if (ttlMillis == null) {
-			ttlMillis = JwtUtil.JWT_TTL;
-		}
-		long expMillis = nowMillis + ttlMillis;
-		Date expDate = new Date(expMillis);
-		return Jwts.builder().id(uuid)// 唯一的ID
-				.header().empty().add(ImmutableMap.of("typ", "JWT", "alg", "HS256")).and().claims(claims)
-				.subject(subject) // 主题 可以是JSON数据
-				.issuer("w2") // 签发者
-				.issuedAt(now) // 签发时间
-				.signWith(secretKey, ALGORITHM) // 使用HS256对称加密算法签名, 第二个参数为秘钥
-				.expiration(expDate);
-	}
+    private static JwtBuilder getJwtBuilder(String subject, Map<String, Object> claims, Long ttlMillis, String uuid) {
+        SecretKey secretKey = generalKey();
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+        if (ttlMillis == null) {
+            ttlMillis = JwtUtil.JWT_TTL;
+        }
+        long expMillis = nowMillis + ttlMillis;
+        Date expDate = new Date(expMillis);
+        return Jwts.builder().id(uuid)// 唯一的ID
+                .header().empty().add(ImmutableMap.of("typ", "JWT", "alg", "HS256")).and().claims(claims)
+                .subject(subject) // 主题 可以是JSON数据
+                .issuer("w2") // 签发者
+                .issuedAt(now) // 签发时间
+                .signWith(secretKey, ALGORITHM) // 使用HS256对称加密算法签名, 第二个参数为秘钥
+                .expiration(expDate);
+    }
 }
