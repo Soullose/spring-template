f9fb802c030001fd94c4da54210fc8e6b65926b6
WangSF <soullose@qq.com>
2025-02-27 23:56:23 +0800

    refactor(framework): 优化 SnowflakeGenerator 类
    - 删除了未使用的导入语句
    - 移除了冗余的注释代码- 简化了 generate 方法的实现

--- src/main/java/com/w2/springtemplate/framework/jpa/SnowflakeGenerator.java
+++ src/main/java/com/w2/springtemplate/framework/jpa/SnowflakeGenerator.java
@@ -1,42 +1,18 @@
 package com.w2.springtemplate.framework.jpa;
 
-import java.io.Serializable;
-
-import cn.hutool.core.util.IdUtil;
 import cn.hutool.extra.spring.SpringUtil;
 import com.w2.springtemplate.framework.fsg.uid.UidGenerator;
 import org.hibernate.HibernateException;
 import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.id.IdentifierGenerator;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-
-//import cn.hutool.core.util.IdUtil;
+import java.io.Serializable;
 
 /// 基于neural 的雪花算法实现
 public class SnowflakeGenerator implements IdentifierGenerator {
-
-//    @Qualifier("cachedUidGenerator")
-//    @Resource
-//    private final UidGenerator uidGenerator;
-//
-//    public SnowflakeGenerator(@Qualifier("cachedUidGenerator") UidGenerator uidGenerator) {
-//        this.uidGenerator = uidGenerator;
-//    }
-
     @Override
     public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
-//        HighConcurrentSnowflake ID_GENERATOR = new HighConcurrentSnowflake();
-//        return Long.toString(ID_GENERATOR.nextId());
-//        SpringUtil.getBean(UidGenerator.class).getUID()
-//        return String.valueOf(SpringUtil.getBean("cachedUidGenerator",UidGenerator.class).getUID());
-
-        return String.valueOf(SpringUtil.getBean("defaultUidGenerator",UidGenerator.class).getUID());
-//        return String.valueOf(HighConcurrentSnowflake.getInstance().nextId());
-//        return IdUtil.fastSimpleUUID();
-//        return SnowflakeIdGenerator.getInstance().nextIdStr();
+        return String.valueOf(SpringUtil.getBean("cachedUidGenerator", UidGenerator.class).getUID());
     }
 
 }
