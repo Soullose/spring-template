5b1a67be8e5dc4234979e895e2581e4fcc5e44da
WangSF <soullose@qq.com>
2025-03-05 23:29:26 +0800

    feat(management): 启用 Spring Boot Actuator 端点- 在 application.yml 中添加 management配置，启用所有端点- 更新 pom.xml，重新排序依赖项并使用变量引用版本号- 在 Knife4jConfiguration 中添加 BeanPostProcessor 以定制 Swagger映射

--- src/main/java/com/w2/springtemplate/config/Knife4jConfiguration.java
+++ src/main/java/com/w2/springtemplate/config/Knife4jConfiguration.java
@@ -1,14 +1,22 @@
 package com.w2.springtemplate.config;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spring.web.plugins.WebMvcRequestHandlerProvider;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+import java.lang.reflect.Field;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2WebMvc
@@ -35,7 +43,7 @@ public class Knife4jConfiguration {
                 .build();
     }
 
-//    @Bean(value = "knife4jDDDConfigurationBean")
+    //    @Bean(value = "knife4jDDDConfigurationBean")
 //    public Docket knife4jDDDConfigurationBean() {
 //        //指定使用Swagger2规范
 //        return new Docket(DocumentationType.SWAGGER_2)
@@ -55,4 +63,33 @@ public class Knife4jConfiguration {
 //                .paths(PathSelectors.any())
 //                .build();
 //    }
+    @Bean
+    public static BeanPostProcessor springfoxHandlerProviderBeanPostProcessor() {
+        return new BeanPostProcessor() {
+
+            @Override
+            public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+                if (bean instanceof WebMvcRequestHandlerProvider) {
+                    customizeSpringfoxHandlerMappings(getHandlerMappings(bean));
+                }
+                return bean;
+            }
+
+            private <T extends RequestMappingInfoHandlerMapping> void customizeSpringfoxHandlerMappings(List<T> mappings) {
+                mappings.removeIf(mapping -> mapping.getPatternParser() != null);
+            }
+
+            @SuppressWarnings("unchecked")
+            private List<RequestMappingInfoHandlerMapping> getHandlerMappings(Object bean) {
+                try {
+                    Field field = ReflectionUtils.findField(bean.getClass(), "handlerMappings");
+                    field.setAccessible(true);
+                    return (List<RequestMappingInfoHandlerMapping>) field.get(bean);
+                } catch (IllegalArgumentException | IllegalAccessException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        };
+    }
+
 }
